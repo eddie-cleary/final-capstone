fd6a63b7bfc148c130ef852dce157e29e8f32309
clearyed <eddiejcleary@gmail.com>
2022-09-04 16:44:16 -0600

    added fetch for all recipes

--- frontend/src/components/Pages/Recipes/AllRecipes.js
+++ frontend/src/components/Pages/Recipes/AllRecipes.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../Layout/Layout";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../../../shared/baseUrl";
+import axios from "axios";
+import { Typography, Stack, List, ListItem } from "@mui/material";
 
 const AllRecipes = () => {
+  const [recipesData, setRecipesData] = useState([]);
+  const token = useSelector((state) => state.auth.token);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl + `/recipes/all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setRecipesData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const recipes = recipesData.map((recipe) => {
+    return <ListItem key={recipe.id}>{recipe.title}</ListItem>;
+  });
+
   return (
     <Layout>
-      <h1>All Recipes</h1>
+      <Typography variant="h2">All Recipes</Typography>
+      <List>
+        <Stack>{recipes}</Stack>
+      </List>
     </Layout>
   );
 };
