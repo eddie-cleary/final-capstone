d79f72a2a467818868d8fb644cb10bfc969c0df1
Kim <99856852+KBryant702@users.noreply.github.com>
2022-09-04 15:21:49 -0700

    Grid sizing added to layout

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { Grid } from "@mui/material"
 
 import React from "react";
 
+
+
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Header />
-      <Sidebar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+
+    <Grid container >
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={3}>
+        <Sidebar />
+      </Grid>
+      <Grid item xs={9}>
+        <main >{children}</main>
+      </Grid>
+      <Grid item xs={12}>
+        <Footer />
+      </Grid>
+    </Grid>
+
+
   );
 };
 
 export default Layout;
+
