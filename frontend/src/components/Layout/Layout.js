411a7dfd463ee5408318fa22b02ff125ce270f6b
Kim <99856852+KBryant702@users.noreply.github.com>
2022-09-05 16:55:18 -0700

    Styling, layout adjustments

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,23 +1,31 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { cardClasses, Grid, Hidden } from "@mui/material";
+import { Grid, Hidden } from "@mui/material";
 
 import React from "react";
 const gridContainer = {
   minWidth: "100vw",
   minHeight: "100vh",
+ 
 };
 
 const header = {
-  height: "200px",
+  height: "100px",
 };
 const sidebar = {
   width: "200px",
+  position: 'fixed',
+  top: '100px',
+
 };
 const main = {
   width: "calc(100vw - 200px)",
-  height: "calc(100vh -300px)",
+  height: "calc(100vh -100px)",
+  position: 'fixed',
+  left: "200px",
+  top: '100px',
+
 };
 const footer = {
   height: "100px",
@@ -29,17 +37,17 @@ const Layout = ({ children }) => {
       <Grid sx={header} item xs={12}>
         <Header />
       </Grid>
-      <Hidden smDown>
-        <Grid item sm>
-          <Sidebar sx={sidebar} />
+      {/* <Hidden smDown> */}
+        <Grid item sm sx={sidebar}>
+          <Sidebar  />
         </Grid>
-      </Hidden>
-      <Hidden mdUp>Insert Collapsible menu</Hidden>
-      <Grid item sm={9}>
-        <main sx={main}>{children}</main>
+      {/* </Hidden> */}
+      {/* <Hidden mdUp>Insert Collapsible menu</Hidden> */}
+      <Grid item sm={9} sx={main}>
+        <main >{children}</main>
       </Grid>
-      <Grid item xs={12}>
-        <Footer sx={footer} />
+      <Grid item xs={12} sx={footer}>
+        <Footer  />
       </Grid>
     </Grid>
   );
