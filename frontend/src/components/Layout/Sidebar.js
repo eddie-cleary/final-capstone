30c8a09cf94387220d25db46950a387568dcddf4
clearyed <eddiejcleary@gmail.com>
2022-09-04 18:31:15 -0600

    formatted sidebar links

--- frontend/src/components/Layout/Sidebar.js
+++ frontend/src/components/Layout/Sidebar.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {Button,  Typography, Stack } from "@mui/material";
+import { Link as ReactLink } from "react-router-dom";
+import { Button, Typography, Stack, Link } from "@mui/material";
 import { deleteUser, addToken } from "../../redux/features/auth/authSlice";
 import { useDispatch } from "react-redux";
 
-
-  
 const Sidebar = () => {
   const dispatch = useDispatch();
   return (
     <Stack>
       <Typography variant="h4">Sidebar</Typography>
-      <Link to="/recipes">Recipes</Link>
-      <Link to="/mealplans">Meal Plans</Link>
+      <Button variant="text">
+        <Link to="/recipes" component={ReactLink} underline="none">
+          View Recipes
+        </Link>
+      </Button>
+      <Button variant="text">
+        <Link to="/mealplans" component={ReactLink} underline="none">
+          My Meal Plans
+        </Link>
+      </Button>
+      <Button variant="text">
+        <Link to="/recipes/add" component={ReactLink} underline="none">
+          Add Recipe
+        </Link>
+      </Button>
       <Button
         onClick={() => {
           dispatch(deleteUser());
