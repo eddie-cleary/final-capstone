ef7d69e21c299f86947f902f29f7836b26782722
clearyed <eddiejcleary@gmail.com>
2022-09-04 21:40:59 -0600

    Added ingredient component to add recipe form

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,54 +1,48 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { cardClasses, Grid, Hidden } from "@mui/material"
+import { cardClasses, Grid, Hidden } from "@mui/material";
 
 import React from "react";
 const gridContainer = {
-  width: '100vw',
-  height: '100vh',
-}
+  minWidth: "100vw",
+  minHeight: "100vh",
+};
 
 const header = {
-  height: '200px',
-}
+  height: "200px",
+};
 const sidebar = {
-  width: '200px',
-}
+  width: "200px",
+};
 const main = {
-  width: 'calc(100vw - 200px)',
-  height: 'calc(100vh -300px)',
-}
+  width: "calc(100vw - 200px)",
+  height: "calc(100vh -300px)",
+};
 const footer = {
-height: '100px',
-}
+  height: "100px",
+};
 
 const Layout = ({ children }) => {
   return (
-
     <Grid container sx={gridContainer}>
-      <Grid item xs={12}>
-        <Header sx={{ header }} />
+      <Grid sx={header} item xs={12}>
+        <Header />
       </Grid>
       <Hidden smDown>
-      <Grid item  sm>
-        <Sidebar sx={ sidebar }/>
-      </Grid>
+        <Grid item sm>
+          <Sidebar sx={sidebar} />
+        </Grid>
       </Hidden>
-      <Hidden mdUp>
-        Insert Collapsible menu
-      </Hidden>
+      <Hidden mdUp>Insert Collapsible menu</Hidden>
       <Grid item sm={9}>
-        <main sx = { main }>{children}</main>
+        <main sx={main}>{children}</main>
       </Grid>
       <Grid item xs={12}>
-        <Footer sx= { footer }/>
+        <Footer sx={footer} />
       </Grid>
     </Grid>
-
-
   );
 };
 
 export default Layout;
-
--- frontend/src/components/Pages/Recipes/AddRecipe.js
+++ frontend/src/components/Pages/Recipes/AddRecipe.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
-  FormControl,
   InputLabel,
   Stack,
   TextField,
   Typography,
-  Select,
-  MenuItem,
   Button,
   FormControlLabel,
   Checkbox,
+  Autocomplete,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import Layout from "../../Layout/Layout";
+import Ingredient from "./Ingredient";
 
 const AddRecipe = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const ingredient = { name: "", quantity: "1", measurement: "1" };
+  const [ingredientList, setIngredientList] = useState([]);
+  const [chosenIngredient, setChosenIngredient] = useState("");
+
+  useEffect(() => {
+    console.log(chosenIngredient);
+  }, [chosenIngredient]);
+
+  const addIngredientToList = () => {
+    const newList = [...ingredientList];
+    let newIngredient = newList[newList.length];
+    newIngredient = { ...ingredient, name: chosenIngredient };
+    newList.push(newIngredient);
+    setIngredientList(newList);
+  };
+
+  const ingredients = ingredientList.map((ingredient, index) => {
+    return (
+      <Ingredient
+        index={index}
+        data={ingredient}
+        ingredientList={ingredientList}
+        setIngredientList={setIngredientList}
+        key={index}
+      />
+    );
+  });
+
+  const allIngredients = ["Garlic", "Salt"];
+
   return (
     <Layout>
       <section>
@@ -23,7 +55,12 @@ const AddRecipe = () => {
             <Typography variant="h5">Add Recipe</Typography>
             <Stack sx={{ mt: 3 }}>
               <InputLabel>Title</InputLabel>
-              <TextField sx={{ mt: 1 }} placeholder="Recipe title"></TextField>
+              <TextField
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                sx={{ mt: 1 }}
+                placeholder="Recipe title"
+              ></TextField>
             </Stack>
             <Stack sx={{ mt: 2 }}>
               <InputLabel>Description</InputLabel>
@@ -32,66 +69,33 @@ const AddRecipe = () => {
                 rows={3}
                 multiline
                 placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
             </Stack>
             <Box sx={{ mt: 3 }}>
-              <Typography sx={{ mb: -2 }}>Ingredients</Typography>
-              <Stack
-                direction="row"
-                sx={{ flexWrap: "nowrap", alignItems: "flex-end" }}
-              >
-                <Stack sx={{ flexGrow: 1 }}>
-                  <InputLabel>&nbsp;</InputLabel>
-                  <TextField placeholder="Ingredient"></TextField>
-                </Stack>
-                <Stack sx={{ mx: 2 }}>
-                  <InputLabel id="quantity">Quantity</InputLabel>
-                  <Select
-                    labelId="quantity"
-                    id="quantity-select"
-                    label="Quantity"
-                    value={1}
-                  >
-                    <MenuItem value={1 / 8}>1/8</MenuItem>
-                    <MenuItem value={1 / 4}>1/4</MenuItem>
-                    <MenuItem value={1 / 3}>1/3</MenuItem>
-                    <MenuItem value={1 / 2}>1/2</MenuItem>
-                    <MenuItem value={2 / 3}>2/3</MenuItem>
-                    <MenuItem value={3 / 4}>3/4</MenuItem>
-                    <MenuItem value={1}>1</MenuItem>
-                    <MenuItem value={2}>2</MenuItem>
-                    <MenuItem value={3}>3</MenuItem>
-                    <MenuItem value={4}>4</MenuItem>
-                    <MenuItem value={5}>5</MenuItem>
-                    <MenuItem value={6}>6</MenuItem>
-                  </Select>
-                </Stack>
-                <Stack>
-                  <InputLabel id="measurement">Measurement</InputLabel>
-                  <Select
-                    labelId="measurement"
-                    id="measurement-select"
-                    label="Measurement"
-                    value={1}
-                  >
-                    <MenuItem value={1}>Teaspoon</MenuItem>
-                    <MenuItem value={3}>Tablespoon</MenuItem>
-                    <MenuItem value={4}>Cup</MenuItem>
-                    <MenuItem value={5}>Pound</MenuItem>
-                    <MenuItem value={6}>Quart</MenuItem>
-                    <MenuItem value={7}>Pint</MenuItem>
-                  </Select>
-                </Stack>
-                <Button variant="outlined" sx={{ height: "56px", ml: 2 }}>
-                  -
-                </Button>
-              </Stack>
+              <Typography>Ingredients</Typography>
+              <Stack>{ingredients}</Stack>
               <Stack direction="row" sx={{ mt: 2 }}>
-                <TextField
+                <Autocomplete
+                  disablePortal
+                  id="ingredient-select"
                   sx={{ flexGrow: 1 }}
-                  placeholder="Ingredient"
-                ></TextField>
-                <Button sx={{ ml: 2 }} variant="outlined">
+                  options={allIngredients}
+                  onChange={(e) => {
+                    if (e.target.textContent) {
+                      setChosenIngredient(e.target.textContent);
+                    }
+                  }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Choose ingredient" />
+                  )}
+                />
+                <Button
+                  onClick={addIngredientToList}
+                  sx={{ ml: 2 }}
+                  variant="outlined"
+                >
                   +
                 </Button>
               </Stack>
