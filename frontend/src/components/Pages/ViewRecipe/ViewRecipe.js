62101a4b854bf679922fd5dff630f0c31a7e8a5d
Brandon Vo <69447326+gitbrandonvo@users.noreply.github.com>
2022-09-07 04:39:37 -0600

    - created recipe page - recipe page displays all quantities, ingredients, and steps
    todos
    - add a dropdown for every ingredient that calls utility function
    
    - useState for props passed in RenderIngredients & RenderStep.
    	- data is not yet fully loaded when initially opening so the page will not render
    	- temporary measure: comment lines 156, 166 & then uncomment to view page.
    
    - add category to appUser obj (missing from JSON)
    
    - fix JSON "ingredient" field (null) & update str
    
    considerations
    
    - ingredient table should store the initial measurement type as a string.
    	- easily convertable & saves tediousness of needing to convert everytime to a certain measurement
    	- if yes (todo): add column to ingredient table & return in appUser obj
    
    - improve design and refactor

--- frontend/src/components/Pages/ViewRecipe/ViewRecipe.js
+++ frontend/src/components/Pages/ViewRecipe/ViewRecipe.js
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../Layout/Layout";
 import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Stack, Card, CardMedia, Box, Grid, Paper, Typography, List, ListItem, ListItemIcon, ListItemText} from "@mui/material";
 import axios from "axios";
 import { baseUrl } from "../../../shared/baseUrl";
 import { useSelector } from "react-redux";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-const SingleRecipe = () => {
+
+const loaded = false;
+
+let RenderIngredients = (props) => {
+  let renderedIngredients = [];
+  for (let i=0; i<props.arr.length; i++) {
+      let currIngredientObj = props.arr[i];
+      const {id:recipeId, quantity:quantityTsp, ingredient:ingredientName} = currIngredientObj;
+      //call utility function that converts tsp passing quantityTsp & return measurement + unit to change string below.
+      renderedIngredients.push("You need: " + quantityTsp + " tsp of " + "ingredient with recipe id of " + recipeId) //todo: repl recipeId str with proper ingredientName when null fixed
+  }
+  return (
+    renderedIngredients.map(ingredient => (
+    <List>
+      <ListItem>
+      <ListItemIcon><AddShoppingCartIcon /></ListItemIcon>
+      <ListItemText primary={ingredient}/>
+      </ListItem>
+    </List>
+  )))
+}
+
+const RenderSteps = (props) => {
+  let renderedSteps = [];
+  for (let i=0; i<props.arr.length; i++) {
+    let currStepObj = props.arr[i];
+    const {info:currentStep} = currStepObj;
+    renderedSteps.push((i+1) + ". " + currentStep)
+  }
+  return (
+    renderedSteps.map(step => (
+      <List>
+        <ListItem>
+        <ListItemIcon><ChevronRightIcon /></ListItemIcon>
+        <ListItemText primary={step}/>
+        </ListItem>
+      </List>
+    )))
+}
+
+
+const SingleRecipe = (props) => {
   const { id } = useParams();
   const [recipe, setRecipe] = useState({});
   const token = useSelector((state) => state.auth.token);
@@ -20,6 +63,7 @@ const SingleRecipe = () => {
       })
       .then((res) => {
         setRecipe(res.data);
+        loaded = true;
       })
       .catch((err) => {
         console.log(err);
@@ -28,11 +72,98 @@ const SingleRecipe = () => {
 
   return (
     <Layout>
-      <section>
-        <Typography variant="h4">{recipe.title}</Typography>
-        <Typography variant="h4">{recipe.description}</Typography>
-        <Typography variant="h4">{recipe.servings}</Typography>
-      </section>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item xs={12}><Paper elevation={5}>
+          <Box display="flex" justifyContent="center" p={2} component="img" src={recipe.imgUrl} alt={`Picture of ${recipe.title}`} sx={{ width: "100vh" }} />
+        </Paper></Grid>
+
+        <Grid item xs={12}><Paper elevation={5}>
+          <Box display="flex" justifyContent="center" p={4} sx={{ width: "100vh" }}>
+            <Typography variant="h3">{recipe.title}</Typography>
+          </Box></Paper></Grid>
+
+        <Grid item xs={12}><Paper elevation={5}>
+          <Box display="flex" justifyContent="center" p={4} sx={{ width: "100vh" }}>
+            <Typography variant="h4">{`Category: ${recipe.category}`}</Typography>
+          </Box></Paper></Grid>
+
+        <Grid item xs={12}><Paper elevation={5}>
+          <Box display="flex" justifyContent="center" p={4} sx={{ width: "100vh" }}>
+            <Typography variant="h4">{`Description: ${recipe.description}`}</Typography>
+          </Box></Paper></Grid>
+
+        <Stack
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+          width="100vh"
+          spacing={2}
+          sx={{ mt: 4 }}
+        >
+          <Card variant="outlined" sx={{ maxWidth: 180 }}>
+            <CardMedia
+              component="img"
+              alt="prepare icon"
+              height="140"
+              image={require("./icons/servings.png")} />
+            <Typography variant="h5" component="div">Servings</Typography>
+            <Typography variant="h5">{`${recipe.prepTime}`}</Typography>
+          </Card>
+
+          <Card variant="outlined" sx={{ maxWidth: 180 }}>
+            <CardMedia
+              component="img"
+              alt="prepare icon"
+              height="140"
+              image={require("./icons/prepare.png")} />
+            <Typography variant="h5" component="div">Prep Time</Typography>
+            <Typography variant="h5">{`${recipe.prepTime} minutes`}</Typography>
+          </Card>
+
+          <Card variant="outlined" sx={{ maxWidth: 180 }}>
+            <CardMedia
+              component="img"
+              alt="prepare icon"
+              height="140"
+              image={require("./icons/cooking.png")} 
+              />
+            <Typography variant="h5" component="div">Cook Time</Typography>
+            <Typography variant="h5">{`${recipe.cookTime} minutes`}</Typography>
+          </Card>
+        </Stack>
+
+  
+        <Stack
+          direction="column"
+          justifyContent = "center"
+          width="100vh"
+          spacing={2}
+          sx={{ mt: 5 }}>
+          <Paper elevation={5}><Box p={4} sx={{ width: "100vh" }}>
+          <Typography variant="h4">Ingredients</Typography>
+          <RenderIngredients arr={recipe.recipeIngredients}/>
+        </Box></Paper>
+        </Stack>
+
+        <Stack
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+        width="100vh"
+        spacing={2}
+        sx={{ mt: 5 }}>
+          <Paper elevation={5}><Box p={4} sx={{ width: "100vh" }}>
+          <Typography variant="h4">Steps</Typography>
+          <RenderSteps arr={recipe.steps}/>
+        </Box></Paper>
+        </Stack> 
+        </Grid>
     </Layout>
   );
 };
