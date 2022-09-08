a901c2dd092e36f0c50c03624b0e5902eafb20ba
clearyed <eddiejcleary@gmail.com>
2022-09-08 15:48:47 -0600

    bugfixes

--- frontend/src/components/Pages/Home/Home.js
+++ frontend/src/components/Pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { addToken, deleteUser } from "../../../redux/features/auth/authSlice";
 import { useSelector, useDispatch } from "react-redux";
 import Layout from "../../Layout/Layout";
-import { Stack, Paper, Link, Button, Typography, Box, autocompleteClasses } from "@mui/material";
+import { Stack, Paper, Link, Button, Typography, Box } from "@mui/material";
 import { Link as ReactLink, useNavigate } from "react-router-dom";
 import Header from "../../Layout/Header";
 
@@ -22,80 +22,97 @@ const Home = () => {
     height: "50px",
     fontWeight: "bold",
     border: "1px solid darkGreen",
-    margin: 'auto',
-    backgroundColor: '#9fcf6f',
+    margin: "auto",
+    backgroundColor: "#9fcf6f",
     "&:hover": {
       boxShadow: 8,
-      backgroundColor: '#71af47',
-
-    }
-  }
+      backgroundColor: "#71af47",
+    },
+  };
   const styles = {
     boxContainer: {
       backgroundImage: `url("https://images.unsplash.com/photo-1636065641424-a9f9493c1e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'top',
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "top",
       height: "calc(100vh - 50px)",
       display: "grid",
-      gridTemplateAreas: `"btnContainer" "collabCredit"`
-    }
-
-  }
+      gridTemplateAreas: `"btnContainer" "collabCredit"`,
+    },
+  };
   const btnContainer = {
     gridArea: "btnContainer",
-    flexDriection: 'column',
-    marginTop: '200px',
-    marginBottom: '20px',
-
-  }
+    flexDriection: "column",
+    marginTop: "200px",
+    marginBottom: "20px",
+  };
 
   const collabCredit = {
-    textAlign: 'center',
-    gridArea: 'collabCredit',
-    fontWeight: 'bold',
-    marginBottom: "50px"
-
-  }
+    textAlign: "center",
+    gridArea: "collabCredit",
+    fontWeight: "bold",
+    marginBottom: "50px",
+  };
   return (
-
     <Box>
       <Header />
       <Box style={styles.boxContainer}>
         <Stack sx={btnContainer}>
-          <Typography variant="h2" textAlign="center" fontWeight="bolder">Get Started</Typography>
+          <Typography variant="h2" textAlign="center" fontWeight="bolder">
+            Get Started
+          </Typography>
           <Button variant="contained" sx={btn}>
-            <Link to="/recipes/add" component={ReactLink} underline="none" color="black">
+            <Link
+              to="/recipes/add"
+              component={ReactLink}
+              underline="none"
+              color="black"
+            >
               Add a New Recipe
             </Link>
           </Button>
           <Button variant="contained" sx={btn}>
-            <Link to="/recipes" component={ReactLink} underline="none" color="black">
+            <Link
+              to="/recipes"
+              component={ReactLink}
+              underline="none"
+              color="black"
+            >
               Browse All Your Recipes
             </Link>
           </Button>
 
           <Button variant="contained" sx={btn}>
-            <Link to="/mealplans" component={ReactLink} underline="none" color="black">
+            <Link
+              to="/mealplans"
+              component={ReactLink}
+              underline="none"
+              color="black"
+            >
               Create a MealPlan
             </Link>
           </Button>
           <Button variant="contained" sx={btn}>
-            <Link to="/mealplans" component={ReactLink} underline="none" color="black">
+            <Link
+              to="/mealplans"
+              component={ReactLink}
+              underline="none"
+              color="black"
+            >
               View All Your MealPlans
             </Link>
           </Button>
           <Box alignContent="center" marginTop="10px">
-            <Typography textAlign='center'>
+            <Typography textAlign="center">
               My Digital Meal Planner &reg; {new Date().getFullYear()} <br />
             </Typography>
             <Typography sx={collabCredit}>
-              Created in Collaboration <br /> by: Edward Cleary, Kimberly Bryant, Chantele Lohr, and Brandon Vo
+              Created in Collaboration <br /> by: Edward Cleary, Kimberly
+              Bryant, Chantele Lohr, and Brandon Vo
             </Typography>
           </Box>
         </Stack>
       </Box>
     </Box>
-
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,14 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { store } from "./redux/store";
-import {CssBaseline} from '@mui/material'
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-      <CssBaseline />
+        <CssBaseline />
         <App />
       </Router>
     </Provider>
