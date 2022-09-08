80ec3b4bd81e4d3cf6e0fbfd00b90a73e47ac0c9
Brandon Vo <69447326+gitbrandonvo@users.noreply.github.com>
2022-09-08 01:17:17 -0600

    - elements and image sizes enforced - added minimal support for converted measurement, needs impl of reference to api (either response needs to change or axios.get) - delete conversion function when import is functional

--- frontend/src/components/Pages/ViewRecipe/ViewRecipe.js
+++ frontend/src/components/Pages/ViewRecipe/ViewRecipe.js
@@ -9,20 +9,166 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import servingsIcon from './icons/servings.png'
 import prepIcon from './icons/prepare.png'
-import cookIcon from './icons/servings.png'
+import cookIcon from './icons/cooking.png'
 
+//////////////////////////////////////////////////////////remove after import is working
+const Fraction = require("fractional").Fraction;
+
+
+function convertToMeasurement(quantity, isLiquid) {
+  const values = getValues(isLiquid);
+
+  let counts = {};
+
+  for (let key in values) {
+    // handle everything from teaspoon and up
+    while (
+      quantity >= values[key] ||
+      (quantity / values[key]).toFixed(2) == 0.75 ||
+      (quantity / values[key]).toFixed(2) == 0.67 ||
+      (quantity / values[key]).toFixed(2) == 0.5 ||
+      (quantity / values[key]).toFixed(2) == 0.33 ||
+      (quantity / values[key]).toFixed(2) == 0.25
+    ) {
+      if (!counts[key]) {
+        counts[key] = "";
+      }
+      if ((quantity / values[key]).toFixed(2) == 0.75) {
+        counts[key] = `${counts[key]} 3/4`;
+        quantity /= values[key];
+        quantity = Math.trunc(quantity);
+        continue;
+      } else if ((quantity / values[key]).toFixed(2) == 0.67) {
+        counts[key] = `${counts[key]} 2/3`;
+        quantity /= values[key];
+        quantity = Math.trunc(quantity);
+        continue;
+      } else if ((quantity / values[key]).toFixed(2) == 0.5) {
+        counts[key] = `${counts[key]} 1/2`;
+        quantity /= values[key];
+        quantity = Math.trunc(quantity);
+        continue;
+      } else if ((quantity / values[key]).toFixed(2) == 0.33) {
+        counts[key] = `${counts[key]} 1/3`;
+        quantity /= values[key];
+        quantity = Math.trunc(quantity);
+        continue;
+      } else if ((quantity / values[key]).toFixed(2) == 0.25) {
+        counts[key] = `${counts[key]} 1/4`;
+        quantity /= values[key];
+        quantity = Math.trunc(quantity);
+        continue;
+      } else if (quantity >= values[key]) {
+        // if the key exists, increase value
+        counts[key]++;
+        quantity = quantity - values[key];
+      }
+      // subtract solid value each time through loop
+    }
+  }
+  // remaining values are less than a teaspoon
+  // convert to a fraction
+
+  if (quantity === 0) {
+    return formatCountsString(counts);
+  }
+
+  const divisor = 1 / Object.entries(values)[2][1];
+
+  let fraction = (quantity * divisor).toString();
+
+  if (fraction == 0) {
+    return formatCountsString(counts);
+  } else {
+    if (fraction.startsWith(0.66)) {
+      fraction = "2/3";
+    } else if (fraction.startsWith(0.33)) {
+      fraction = "1/3";
+    } else {
+      fraction = new Fraction(fraction);
+      fraction = fraction.toString();
+    }
+
+    if (!isLiquid) {
+      if (counts.teaspoon) {
+        counts.teaspoon =
+          counts.teaspoon + ` ${fraction === 0 ? "" : fraction}`;
+      } else {
+        // teaspoons don't exist, create teaspoon key and add remaining fraction
+        counts.teaspoon = fraction === 0 ? "" : fraction;
+      }
+    } else {
+      if (counts.ounce) {
+        counts.ounce = counts.ounce + ` ${fraction === 0 ? "" : fraction}`;
+      } else {
+        // teaspoons don't exist, create teaspoon key and add remaining fraction
+        counts.ounce = fraction === 0 ? "" : fraction;
+      }
+    }
+  }
+
+  return formatCountsString(counts);
+};
+
+export const calculateQuantity = (number, fraction, unitOfMeasure) => {
+  const allValues = {
+    teaspoon: 8,
+    tablespoon: 24,
+    ounce: 48,
+    cup: 384,
+    pint: 768,
+    quart: 1536,
+  };
+
+  const measurement = unitOfMeasure.toLowerCase();
+  const currQuantity = allValues[measurement];
+
+  return (
+    (number ? number * currQuantity : 0) +
+    (fraction ? fraction * currQuantity : 0)
+  );
+};
+
+const formatCountsString = (countsObj) => {
+  let newString = "";
+  for (let key in countsObj) {
+    newString += `${countsObj[key]} ${key} `;
+  }
+  return newString.replace(/\s+/g, " ").trim();
+};
+
+const getValues = (isLiquid) => {
+  const solidValues = {
+    cup: 384,
+    tablespoon: 24,
+    teaspoon: 8,
+  };
+  const liquidValues = {
+    quart: 1536,
+    pint: 768,
+    ounce: 48,
+  };
+
+  if (isLiquid) {
+    return liquidValues;
+  } else {
+    return solidValues;
+  }
+};
+
+///////////////////////////////////////////////////////////////////////
 
 let RenderIngredients = ({ ingredients }) => {
   let renderedIngredients = [];
   ingredients?.map((ingredient) => {
     const { id: recipeId, quantity: quantityTsp, ingredient: ingredientName } = ingredient;
-    //call utility function that converts tsp passing quantityTsp & return measurement + unit to change string below.
-    renderedIngredients.push("You need: " + quantityTsp + " tsp of " + "ingredient with recipe id of " + recipeId) //todo: repl recipeId str with proper ingredientName when null fixed
-
+    let isLiquid = false; //either axios.get, or change the obj returned from endpoint.
+    let convertedMeasurement = convertToMeasurement(quantityTsp, isLiquid)
+    renderedIngredients.push(convertedMeasurement + " of " + ingredientName) 
   });
   return (
     renderedIngredients.map(ingredient => (
-      <List>
+      <List key={ingredient}>
         <ListItem>
           <ListItemIcon><AddShoppingCartIcon /></ListItemIcon>
           <ListItemText primary={ingredient} />
@@ -40,7 +186,7 @@ const RenderSteps = ({ steps }) => {
   });
   return (
     renderedSteps.map(step => (
-      <List>
+      <List key={step}>
         <ListItem>
           <ListItemIcon><ChevronRightIcon /></ListItemIcon>
           <ListItemText primary={step} />
@@ -49,15 +195,15 @@ const RenderSteps = ({ steps }) => {
     )))
 }
 
-const TimeCard = (props) => {
+const InfoCard = (props) => {
   return (
-    <Card variant="outlined" sx={{ maxWidth: 180 }}>
+    <Card variant="outlined" sx={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 180 }}>
       <CardMedia
         component="img"
         alt="prepare icon"
         height="140"
         image={props.img} />
-      <Typography variant="h5" component="div">{props.name}</Typography>
+      <Typography variant="h5" component="div"><BoldUnderline text={props.name} /></Typography>
       <Typography variant="h5">{props.text}</Typography>
     </Card>
   )
@@ -66,9 +212,15 @@ const TimeCard = (props) => {
 const GridItem = ({ title }) => {
   return (
     <Grid item xs={12}><Paper elevation={5}>
-      <Box display="flex" justifyContent="center" p={4} sx={{ width: "100vh" }}>
+      <Box display="flex" justifyContent="center" alignItems="center" p={4} sx={{ width: "38vw" }}>
         <Typography variant="h3">{title}</Typography>
       </Box></Paper></Grid>
+  )
+}
+
+const BoldUnderline = ({ text }) => {
+  return (
+    <Box fontWeight="fontWeightBold" textDecoration= "underline">{text}</Box>
   )
 }
 
@@ -95,7 +247,7 @@ const SingleRecipe = ({ props }) => {
       });
   }, []);
 
-  // console.log(JSON.stringify({ recipe }))
+  console.log(JSON.stringify({ recipe }))
 
   return (
     <Layout>
@@ -107,37 +259,37 @@ const SingleRecipe = ({ props }) => {
         spacing={3}
       >
         <Grid item xs={12}><Paper elevation={5}>
-          <Box display="flex" justifyContent="center" p={2} component="img" src={recipe.imgUrl} alt={`Picture of ${recipe.title}`} sx={{ width: "100vh" }} />
+          <Box display="flex" justifyContent="center" p={2} component="img" src={recipe.imgUrl} alt={`Picture of ${recipe.title}`} sx={{ aspectRatio: "6/4", width: "38vw", maxHeight: "68vh", objectFit: "cover" }} />
         </Paper></Grid>
 
         <GridItem title={recipe.title} />
-        <GridItem title={`Category: ${recipe.category}`} />
+        {/* <GridItem title={`Category: ${recipe.category}`} /> */}
         <GridItem title={recipe.description} />
 
         <Stack
           direction="row"
           justifyContent="space-evenly"
           alignItems="center"
-          width="100vh"
-          spacing={2}
+          width="38vw"
+          spacing={4}
           sx={{ mt: 4 }}>
-          <TimeCard name="Servings" text={`${recipe.servings}`} img={servingsIcon} />
-          <TimeCard name="Prep Time" text={recipePrepTime == 1 ? "1 minute" : `${recipePrepTime} minutes`} img={prepIcon} />
-          <TimeCard name="Cook Time" text={recipeCookTime == 1 ? "1 minute" : `${recipeCookTime} minutes`} img={cookIcon} />
+          <InfoCard name="Servings" text={`${recipe.servings}`} img={servingsIcon} />
+          <InfoCard name="Prep time" text={recipePrepTime == 1 ? "1 minute" : `${recipePrepTime} minutes`} img={prepIcon} />
+          <InfoCard name="Cook time" text={recipeCookTime == 1 ? "1 minute" : `${recipeCookTime} minutes`} img={cookIcon} />
         </Stack>
 
         <Stack
           direction="column"
           justifyContent="center"
-          width="100vh"
+          width="38vw"
           spacing={2}
           sx={{ mt: 5 }}>
-          <Paper elevation={5}><Box p={4} sx={{ width: "100vh" }}>
+          <Paper elevation={5}><Box p={4}>
             <Typography variant="h4">Ingredients</Typography>
             <RenderIngredients ingredients={recipe.recipeIngredients} />
           </Box>
-        </Paper>
-      </Stack>
+          </Paper>
+        </Stack>
 
         <Stack
           direction="column"
@@ -145,8 +297,8 @@ const SingleRecipe = ({ props }) => {
           alignItems="center"
           width="100vh"
           spacing={2}
-          sx={{ mt: 5 }}>
-          <Paper elevation={5}><Box p={4} sx={{ width: "100vh" }}>
+          sx={{ mt: 5, mb: 4}}>
+          <Paper elevation={5}><Box p={4} sx={{ width: "38vw" }}>
             <Typography variant="h4">Steps</Typography>
             <RenderSteps steps={recipe.steps} />
           </Box></Paper>
