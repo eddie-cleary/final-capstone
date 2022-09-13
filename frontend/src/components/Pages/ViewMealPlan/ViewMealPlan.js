c454642c62f0ee7ebd28213719f454dacba7b1a6
clearyed <eddiejcleary@gmail.com>
2022-09-13 09:37:34 -0600

    added modal for delete meal plan

--- frontend/src/components/Pages/ViewMealPlan/ViewMealPlan.js
+++ frontend/src/components/Pages/ViewMealPlan/ViewMealPlan.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { baseUrl } from "../../../shared/baseUrl";
 import { useSelector } from "react-redux";
 import Layout from "../../Layout/Layout";
-import { Link as ReactLink } from "react-router-dom";
+import { Link as ReactLink, useNavigate } from "react-router-dom";
 import {
   Typography,
   ListItem,
@@ -14,6 +14,9 @@ import {
   Stack,
   Modal,
   Box,
+  Snackbar,
+  Alert,
+  CircularProgress,
 } from "@mui/material";
 import SingleRecipe from "../../shared/SingleRecipe/SingleRecipe";
 
@@ -33,12 +36,33 @@ const modalStyles = {
   overflow: "scroll",
 };
 
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  backgroundColor: "#fff",
+  padding: "15px",
+};
+
 const ViewMealPlan = () => {
   const { id } = useParams();
   const token = useSelector((state) => state.auth.token);
   const [mealPlan, setMealPlan] = useState(null);
   const [showRecipeModal, setShowRecipeModal] = useState(false);
   const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [successMsg, setSuccessMsg] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -66,6 +90,32 @@ const ViewMealPlan = () => {
     console.log("recipe modal is showing ", showRecipeModal);
     console.log("current recipe is ", currentRecipe);
   }, [showRecipeModal]);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    setShowDeleteModal(true);
+    return;
+  };
+
+  const deleteMealPlan = () => {
+    setIsLoading(true);
+    axios
+      .delete(baseUrl + `/mealplans/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setSuccessMsg("Meal plan deleted!");
+        setShowSuccess(true);
+        navigate("/mealplans");
+      })
+      .catch((err) => {
+        setErrMsg(`${err.response}`);
+        setShowError(true);
+      })
+      .then(() => setIsLoading(false));
+  };
 
   const MealRecipe = ({ mealRecipe }) => {
     return (
@@ -115,19 +165,90 @@ const ViewMealPlan = () => {
 
   return (
     <Layout>
-      <Stack>{dayComponents}</Stack>
       <Box>
         <Link to={`/mealplans/edit/${mealPlan?.id}`} component={ReactLink}>
           <Button variant="contained" sx={{ width: "auto" }}>
             Edit Meal Plan
           </Button>
         </Link>
+        <Button
+          onClick={handleDelete}
+          color="warning"
+          variant="contained"
+          sx={{ width: "auto", ml: 2 }}
+        >
+          {isLoading ? <CircularProgress /> : "Delete Meal Plan"}
+        </Button>
       </Box>
+      <Stack sx={{ mt: 4 }}>{dayComponents}</Stack>
       <Modal keepMounted open={showRecipeModal} onClose={handleCloseModal}>
         <Stack sx={modalStyles}>
           <SingleRecipe recipe={currentRecipe} />
         </Stack>
       </Modal>
+
+      <Modal
+        open={showDeleteModal}
+        keepMounted
+        onClose={() => setShowDeleteModal(false)}
+        aria-labelledby="modal-register"
+        aria-describedby="modal-register"
+      >
+        <Box style={modalStyle}>
+          <form>
+            <Stack alignItems="center">
+              <Typography
+                sx={{ textAlign: "center" }}
+              >{`Are you sure you want to delete mealplan: ${mealPlan?.title}?`}</Typography>
+              <Stack direction="row" sx={{ mt: 2 }}>
+                <Button
+                  variant="contained"
+                  onClick={() => setShowDeleteModal(false)}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  sx={{ ml: 2 }}
+                  variant="contained"
+                  onClick={deleteMealPlan}
+                  color="warning"
+                >
+                  Delete
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+        </Box>
+      </Modal>
+
+      <Snackbar
+        open={showSuccess}
+        autoHideDuration={5000}
+        onClose={() => setShowSuccess(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert
+          onClose={() => setShowSuccess(false)}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {successMsg}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={showError}
+        autoHideDuration={5000}
+        onClose={() => setShowError(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert
+          onClose={() => setShowError(false)}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          {errMsg}
+        </Alert>
+      </Snackbar>
     </Layout>
   );
 };
