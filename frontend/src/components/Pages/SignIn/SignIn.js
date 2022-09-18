6e378692f5b2ea91143a5a7eb7003c4fb03eb4d4
clearyed <eddiejcleary@gmail.com>
2022-09-18 13:27:43 -0600

    styling changes

--- frontend/src/components/Pages/SignIn/SignIn.js
+++ frontend/src/components/Pages/SignIn/SignIn.js
@@ -25,59 +25,62 @@ const SignIn = () => {
 
   return (
     <Stack alignItems="center" sx={{ minHeight: "100vh" }}>
-      <Box component="header" sx={headerStyles}>
-        <Box
-          component="img"
-          src={logo}
-          sx={{ height: matches ? "120px" : "100px" }}
-          alt="Meal planner logo"
-        />
-      </Box>
-      <Box component="main" sx={{ flexGrow: 1 }}>
-        <Box
-          sx={{
-            maxWidth: "450px",
-            width: "100%",
-            mt: matches ? 4 : 10,
-            mx: "auto",
-          }}
-        >
-          {showCreateAccount ? (
-            <CreateAccount setShowCreateAccount={setShowCreateAccount} />
-          ) : (
-            <Login setShowCreateAccount={setShowCreateAccount} />
-          )}
+      <Stack sx={{ p: 3 }}>
+        <Box component="header" sx={headerStyles}>
+          <Box
+            component="img"
+            src={logo}
+            sx={{ height: matches ? "120px" : "100px" }}
+            alt="Meal planner logo"
+          />
         </Box>
-        <Stack alignItems="center">
-          <Stack
-            direction="row"
-            justifyContent="center"
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Box
             sx={{
+              maxWidth: "450px",
               width: "100%",
-              flexWrap: "wrap",
-              gap: "40px",
-              mb: 13,
-              mt: 7,
+              mt: matches ? 4 : 3,
+              mx: "auto",
+              transform: matches ? "" : "scale(0.8)",
             }}
           >
-            <FeatureCard
-              image={mealplanImg}
-              title={"Meal Planning"}
-              text={"Create and print meal plans"}
-            />
-            <FeatureCard
-              image={recipesImg}
-              title={"Recipes"}
-              text={"Save, edit, and share recipes"}
-            />
-            <FeatureCard
-              image={shoppingListImg}
-              title={"Grocery Lists"}
-              text={"Generate grocery lists for easy shopping"}
-            />
+            {showCreateAccount ? (
+              <CreateAccount setShowCreateAccount={setShowCreateAccount} />
+            ) : (
+              <Login setShowCreateAccount={setShowCreateAccount} />
+            )}
+          </Box>
+          <Stack alignItems="center">
+            <Stack
+              direction="row"
+              justifyContent="center"
+              sx={{
+                width: "100%",
+                flexWrap: "wrap",
+                gap: "40px",
+                mb: 13,
+                mt: 7,
+              }}
+            >
+              <FeatureCard
+                image={mealplanImg}
+                title={"Meal Planning"}
+                text={"Create and print meal plans"}
+              />
+              <FeatureCard
+                image={recipesImg}
+                title={"Recipes"}
+                text={"Save, edit, and share recipes"}
+              />
+              <FeatureCard
+                image={shoppingListImg}
+                title={"Grocery Lists"}
+                text={"Generate grocery lists for easy shopping"}
+              />
+            </Stack>
           </Stack>
-        </Stack>
-      </Box>
+        </Box>
+      </Stack>
       <Footer />
       <ErrorDisplay />
     </Stack>
