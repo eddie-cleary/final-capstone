d5d427399485a77d31a9e56a13466e223bfaed69
clearyed <eddiejcleary@gmail.com>
2022-09-18 17:11:49 -0600

    bugfix

--- java/src/main/java/com/techelevator/config/DevConfig.java
+++ java/src/main/java/com/techelevator/config/DevConfig.java
@@ -13,7 +13,9 @@ public class DevConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "PUT", "DELETE", "GET");
     }
 }
 
